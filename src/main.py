cdeff3d4910f843d8ab41c0aa3fef663185dcb88
maniakalochka <prochorwolochoff@gmail.com>
2024-08-15 18:27:42 +0300

    Добавлены модели Pydantic для каждой таблицы

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 
 from fastapi import FastAPI
 import uvicorn
+from sqlalchemy.orm import Session
+from typing import List
+
+from models.books import Book as BookModel
+from models.authors import Author as AuthorModel
+from models.users import User as UserModel
+
 
 app = FastAPI()
+
 
 
 if __name__ == "__main__":
