8190fb4cf18f26713b01d1f49c0cd2143d462597
maniakalochka <prochorwolochoff@gmail.com>
2024-08-15 16:46:18 +0300

    Добавлены модели: модели пользователей, авторов книг и собственно сами книги

---
+++ src/db/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+DB_USER = os.getenv("DB_USERNAME")
+DB_PASS = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_PORT = os.getenv("DB_PORT")
+
+DATABSE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_async_engine(DATABSE_URL, echo=True)
+
+
