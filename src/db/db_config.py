12b10bf28284f9595eb7545660edf77739e16bd6
maniakalochka <prochorwolochoff@gmail.com>
2024-08-21 13:41:29 +0300

    Добавлена документация для db_config

--- src/db/db_config.py
+++ src/db/db_config.py
@@ -6,9 +6,9 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
+load_dotenv()  # загрузка .env
 
-DB_USER = os.getenv("DB_USER")
+DB_USER = os.getenv("DB_USER") 
 DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
@@ -18,19 +18,22 @@ DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{D
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
+# фабрика сессий
 SessionLocal = sessionmaker(
     autocommit=False,   
     autoflush=False,
     bind=engine,
     class_=AsyncSession)
 
-Base = declarative_base()
+Base = declarative_base()  # создает базовый класс для определения моделей БД (SQLalchemy)
 
 
 async def get_db():
+    """получение сессии БД в асинхронном контексте."""
     async with SessionLocal() as session:
         yield session
 
 async def init_db():
+    """Асинхронная инициализация БД"""
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
