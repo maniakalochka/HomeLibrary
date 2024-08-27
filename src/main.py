149931efb2b95f79a861ff70da870b2ba3d43e8e
maniakalochka <prochorwolochoff@gmail.com>
2024-08-27 11:11:21 +0300

    удалена возможность auth с помощью fastapi-users

--- src/main.py
+++ src/main.py
@@ -1,62 +1,25 @@
 import uvicorn  # Веб-вервер для Python
 import asyncio 
 import json
-from auth.manager import auth_backend, current_active_user
 from db.db_config import init_db
 from fastapi import FastAPI, Depends
 
 from models.model_user import User 
 from fastapi_users import fastapi_users, FastAPIUsers
 from db.db_config import init_db
-from auth.manager import get_user_manager
 
 from schemas.schema_user import UserCreate, UserRead, UserUpdate
 from routers import router_author, router_book, router_user
-from auth.manager import fastapi_users
 
 
 app = FastAPI()
 
-# with open("/home/maniakalochka/openapi.json") as f:
-#     custom_openapi = json.load(f)
-
-# app.openapi_schema = custom_openapi
-
-fastapi_users = FastAPIUsers[User, int](  
-    get_user_manager,
-    [auth_backend],
-)  # Модель пользователя, позволяет создавать, обновлять, удалять и искать пользователей
 
 
-# подключение маршрутизаторов к приложению, управляют юзерами и auth
-app.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate),
-    prefix="/auth",
-    tags=["auth"],
-)
-app.include_router(
-    fastapi_users.get_reset_password_router(),
-    prefix="/auth",
-    tags=["auth"],
-)
-app.include_router(
-    fastapi_users.get_verify_router(UserRead),
-    prefix="/auth",
-    tags=["auth"],
-)
-app.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
-    prefix="/users",
-    tags=["users"],
-)
-# маршрутизация для авторов и книг
 app.include_router(router_author.router, prefix="/authors", tags=["authors"]) 
 app.include_router(router_book.router, prefix="/books", tags=["books"])
 
-@app.get("/authenticated-route")
-async def authenticated_route(user: User = Depends(current_active_user)):
-    """Защищенный маршрут для GET"""
-    return {"message": f"Hello {user.email}!"}
+
 
 
 @app.on_event("startup")  # функция on_startup вызывает функции для события
--- src/routers/router_user.py
+++ src/routers/router_user.py
@@ -5,21 +5,9 @@ from sqlalchemy.orm import Session
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
 
-from auth.manager import UserManager
 from models.model_user import User as SQLAlchemyUser
 from schemas.schema_user import  UserCreate, UserRead
 from db.db_config import get_db
 from service.service_user import create_user, get_users
 
 router = APIRouter()
-
-@router.post("/", response_model=UserRead)
-async def create_user_endpoint(user: UserCreate, db: AsyncSession = Depends(get_db)):
-    user_manager = UserManager(db)
-    return user_manager.create_user(user=user)
-
-@router.get("/", response_model=List[UserRead])
-async def get_users_endpoint(skip: int = 0, limit: int = 10, db: AsyncSession = Depends(get_db)):
-
-    user_manager = UserManager(db)
-    return await user_manager.get_users(db=db, skip=skip, limit=limit)
